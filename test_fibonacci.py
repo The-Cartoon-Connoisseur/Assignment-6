c306ec5429d06764f50c2fa54e3e78b2488fabee
Lucas Hester <lucashester@icloud.com>
2025-04-04 12:23:57 -0400

    Test 2
    Given a constructor value of 0, the iterator should produce the values [0] if cast as a list.

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -5,3 +5,8 @@ from fibonacci import Fibonacci
 def test_fibonacci_sequence():
     fib = Fibonacci(6)
     assert list(fib) == [0, 1, 1, 2, 3, 5]
+
+#Test 2
+def test_zero_terms():
+    fib = Fibonacci(0)
+    assert list(fib) == []
